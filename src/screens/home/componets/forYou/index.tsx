1d2f8a1aa44e34b9407899ff21c359873a814be7
Fábio <fabio@parceiro.com.br>
2024-11-20 20:13:53 -0400

    adicionado fonts

--- src/screens/home/componets/forYou/index.tsx
+++ src/screens/home/componets/forYou/index.tsx
@@ -1,51 +1,45 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 const ForYou = () => {
-    return (
-        <View style={styles.containerForYou}>
-            <View style={styles.headerForYou}>
-                <View>
-                    <Text style={StyleSheet.flatten([{ color: '#835525', fontWeight: 900 }])}>Para você</Text>
-                </View>
-                <View style={styles.buttonsLeftRight}>
-                    <View>
-                        <TouchableOpacity style={styles.spacebutton}>
-                            <Image source={require("../../../../assets/left.png")} style={styles.image} />
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <TouchableOpacity>
-                            <Image source={require("../../../../assets/right.png")} style={styles.image} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-
+  return (
+    <View style={styles.containerForYou}>
+      <View style={styles.headerForYou}>
+        <View>
+          <Text
+            style={StyleSheet.flatten([{color: '#835525', fontWeight: 900}])}>
+            Para você
+          </Text>
         </View>
-
-
-    )
-
-
-
-}
+        <View style={styles.buttonsLeftRight}>
+          <View>
+            <TouchableOpacity style={styles.spacebutton}>
+              <Image source={require('../../../../assets/left.png')} />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity>
+              <Image source={require('../../../../assets/right.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    containerForYou: {
-        marginTop: 40
-    },
-    buttonsLeftRight: {
-        flexDirection: "row",
-
-    },
-    headerForYou: {
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    spacebutton: {
-        marginHorizontal: 10
-    },
-
-})
+  containerForYou: {
+    marginTop: 40,
+  },
+  buttonsLeftRight: {
+    flexDirection: 'row',
+  },
+  headerForYou: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  spacebutton: {
+    marginHorizontal: 10,
+  },
+});
 export default ForYou;
